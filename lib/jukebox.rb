9bee9a7402188effbea663eb4c9bd24c59f0d550
feven <feventsegay6@gmail.com>
2019-05-08 13:32:15 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -9,4 +10,55 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+def list(songs)
+  songs.each do |song|
+    puts song
+  end
+end
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_request=gets.chomp
+  songs.each do |song|
+    if user_request == song
+    puts "playing #{songs}"
+  
+
+else 
+  puts "Invalid input, please try again"
+end
+end
+end
+
+
+def exit_jukebox 
+  puts "Goodbye."
+end
+def run(songs)
+  help()
+  puts "Please enter a command:"
+  user_response=gets.chomp
+  if user_response=="exit"
+    exit_jukebox()
+  end
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
